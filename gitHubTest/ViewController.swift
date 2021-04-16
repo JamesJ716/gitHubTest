6f5a5e6546963d79f1fcb1c8f30b9c398250c83f
James Jermann <jamesjermann@gmail.com>
2021-04-16 12:45:42 -0400

    Added Mr. Smith

--- gitHubTest/ViewController.swift
+++ gitHubTest/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 var whoIs = "talking in class"
+    var whoIsThat = "its Mr. Smith"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
