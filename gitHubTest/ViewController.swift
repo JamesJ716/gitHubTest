ba6a4183105a8506ea0fadc1b9f5aaf9a7e793e7
James Jermann <jamesjermann@gmail.com>
2021-04-16 12:42:14 -0400

    Initial Commit

---
+++ gitHubTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gitHubTest
+//
+//  Created by James Jermann on 4/16/21.
+//  Copyright © 2021 James Jermann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+var whoIs = "talking in class"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(whoIs)
+    }
+
+
+}
+
